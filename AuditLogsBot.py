65fc3bed1e491203780ccb4ae60b7add804cdfc9
Leandro <a@l5.lol>
2024-06-19 11:17:07 +0200

    20240619-1119
    Added TimeZone and other things

--- AuditLogsBot.py
+++ AuditLogsBot.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
 import json
+from datetime import timedelta
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
-AUDIT_LOG_PREFIX = "[Audit Log]"
 
 # Load configuration from config.json
 with open('config.json', 'r') as f:
@@ -30,8 +30,14 @@ async def print_audit_log(entry):
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
         time_format = "%Y-%m-%d %H:%M:%S"
-        timestamp = entry.created_at.strftime(time_format)
-        username = entry.user.name
+        original_timestamp = discord.utils.snowflake_time(entry.id)
+        
+        # Adjust the timestamp using the timezone offset from config.json
+        timezone_offset = config.get('timezone_offset', 0)
+        adjusted_timestamp = original_timestamp + timedelta(hours=timezone_offset)
+        
+        timestamp = adjusted_timestamp.strftime(time_format)
+        # username = entry.user.name
         action = entry.action
         target = entry.target
         
@@ -42,14 +48,15 @@ async def print_audit_log(entry):
         embed = discord.Embed(
             title="Audit Log",
             description=f"User: {user_mention}\n"  # Mention the user
-                        f"Time: {timestamp}\n"
+                        # f"Username: {username} \n"
+                        f"Time: {timestamp} UTC {timezone_offset:+}\n"  # Display the offset
                         f"Entry action: {action}\n"
                         f"Target action: {target}\n",
             color=discord.Color.from_rgb(config['color']['audit_log']['r'], config['color']['audit_log']['g'], config['color']['audit_log']['b'])
         )
         
         embed.set_thumbnail(url=user_avatar_url)  # Set user's avatar as thumbnail
-        embed.set_footer(text="Made by L5n")
+        # embed.set_footer(text="Made by L5n")
         
         await channel.send(embed=embed)
 
@@ -59,15 +66,16 @@ async def on_ready():
     bot.loop.create_task(on_guild_audit_log_task())
 
 async def on_guild_audit_log_task():
-    last_entries = []
+    last_entry_id = None
     while True:
         guild = bot.guilds[0]
         new_entries = await get_audit_logs(guild, limit=1)
         if new_entries:
-            if new_entries[0] not in last_entries:
-                last_entries.append(new_entries[0])
-                await print_audit_log(new_entries[0])
-        await asyncio.sleep(10)  # Wait for 10 seconds before checking for new entries
+            new_entry = new_entries[0]
+            if new_entry.id != last_entry_id:
+                await print_audit_log(new_entry)
+                last_entry_id = new_entry.id
+        await asyncio.sleep(1)  # Wait for 1 second before checking for new entries
 
 async def get_audit_logs(guild, limit=None):
     audit_logs = guild.audit_logs(limit=limit)
